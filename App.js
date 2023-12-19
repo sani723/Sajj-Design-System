2fbecb717c48b1c4a148c353c81d7c5d429c115a
Sajjad Haider <sani723@hotmail.com>
2023-12-20 02:03:53 +0400

    Update the screen styles to taulwind

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import Constants from 'expo-constants';
 import Storybook from './.storybook';
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -14,11 +14,3 @@ const App = () => {
 
 export default Constants.expoConfig?.extra?.storybookEnabled ? Storybook : App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
